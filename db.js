8d16b655f369115485a206aedcf4cd9dde631c5e
ProstoSanya27 <prosto.sanya27@gmail.com>
2022-09-28 16:00:37 +0300

    end test

--- db.js
+++ db.js
@@ -1,12 +1,23 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize(
+const args = process.env.NODE_ENV === 'test' ?
+  [
+    process.env.DB_TEST_NAME,
+    process.env.DB_TEST_USER,
+    process.env.DB_TEST_PASS
+  ]
+  :
+  [
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-    }
-)
+    process.env.DB_PASS
+  ]
+
+args.push({
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    logging: false
+})
+
+module.exports = new Sequelize(...args)
